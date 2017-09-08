f060794354967e1fa510c3fde87f8fe6ee618eb8
Rashmipriya11 <rashmipriya1102@gmail.com>
2017-09-08 18:07:51 +0530

    Add exsisting file

--- send.php
+++ send.php
@@ -8,7 +8,7 @@ $city = $_POST['city'];
 $pincode = $_POST['pincode'];
 $message = $_POST['message'];
 $formcontent=" From: Name: $name \n Phone: $phone \n Multiple System Operator: $call \n States: $states \n City: $city \n Pincode: $pincode \n Message: $message";
-$recipient = "Subash3040@gmail.com";
+$recipient = "info@5gcity.in";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
